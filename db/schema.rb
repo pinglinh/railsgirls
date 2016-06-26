434cd66a675aa7a98a134f97213220690c74e2e7
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-06-26 13:10:35 +0100

    Visible comments section, resized idea thumbnails, added user ID to ideas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625153106) do
+ActiveRecord::Schema.define(version: 20160625154621) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "user_name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160625153106) do
     t.string   "picture"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
